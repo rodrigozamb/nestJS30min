b7c65b143d742786226247bbd3fc521d271d0e1d
rodrigo.zamboni <rodrigo.zamboni@datasprits.com>
2022-01-03 11:07:50 -0300

    port..

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { UsersModule } from './users/users.module';
 import {MongooseModule} from '@nestjs/mongoose'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://rodrigozamb:rodrigo123@nestjs30min.w2fc9.mongodb.net/test'),UsersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://rodrigozamb:rodrigo123@cluster0.w2fc9.mongodb.net/test'),UsersModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port: number = parseInt(`${process.env.PORT}`) || 8000;
-  console.log("Listening on port = ",port)
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
