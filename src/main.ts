34953dfc06f98264a11a702692621043dce62f40
rodrigo.zamboni <rodrigo.zamboni@datasprits.com>
2022-01-03 10:18:12 -0300

    fix process port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port: number = parseInt(`${process.env.PORT}`) || 8000;
+  console.log("Listening on port = ",port)
   await app.listen(port);
 }
 bootstrap();
